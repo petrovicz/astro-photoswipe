fb7a97377dd31bac380c7b59c54c2914a735e5e4
Petrovicz <benedek.petrovicz@gmail.com>
2024-04-04 14:44:26 +0200

    Setting custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://petrovicz.github.io',
-	base: '/astro-photoswipe'
+	site: 'https://astro-photoswipe.petrovicz.com',
+	base: '/'
 });
