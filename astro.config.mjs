55250a6fbe0960455594bf9f3275c8891e56ca4f
Petrovicz <benedek.petrovicz@gmail.com>
2024-04-04 14:46:10 +0200

    Removing base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://astro-photoswipe.petrovicz.com',
-	base: '/'
+	site: 'https://astro-photoswipe.petrovicz.com'
 });
