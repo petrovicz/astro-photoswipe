c6b7c6fe51e944b79394302c43e6726c5a112b2d
Petrovicz <benedek.petrovicz@gmail.com>
2024-04-09 15:30:37 +0200

    Adding Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	site: 'https://astro-photoswipe.petrovicz.com'
+  integrations: [tailwind(), partytown()],
+  site: 'https://astro-photoswipe.petrovicz.com'
 });
